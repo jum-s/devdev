43e3e2058abb1624e7d064ab6bdf4b867f8a9333
jum-s <vincent.jumeaux@gmail.com>
2014-07-28 11:39:59 +0200

    autoposts views ok

--- app/controllers/autoposts_controller.rb
+++ app/controllers/autoposts_controller.rb
@@ -1,9 +1,10 @@
 class AutopostsController < ApplicationController
 	def index
-		@autoposts = Autopost.order("pocket_date DESC").all
-	end
+    @autoposts = Autopost.order("pocket_date DESC").all
+  end
 
-	def show
-		@autopost = Autopost.find(params[:id])
+  def show
+    @autopost = Autopost.find(params[:id])
+		@next_autoposts = Autopost.order("pocket_date DESC").last(4)
 	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  # Estimation of the time to read a post
+  # Merci Em-AK
+  def get_reading_time(post)
+    words_per_minute = 170.0
+    time = post.word_count / words_per_minute
+    reading_time = "#{time.round} min"
+    if time < 1
+            reading_time = "< 1 min"
+    elsif time >= 1 && time < 1.6
+            reading_time = "1 min"
+    end
+    reading_time
+  end
+
+  # Date
+  def get_date(post)
+    post.created_at.strftime("%e %b.")
+  end
 end
