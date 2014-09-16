3ffad62e20e46478e861ad8f282f21d6da0d328b
jum-s <vincent.jumeaux@gmail.com>
2014-09-16 16:51:13 +0200

    mend

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,16 @@
 module ApplicationHelper
-  # # Estimation of the time to read a post, Merci Em-AK !
-  # def get_reading_time(post)
-  #   words_per_minute = 170.0
-  #   time = post.word_count / words_per_minute
-  #   reading_time = "#{time.round} min"
-  #   if time < 1
-  #           reading_time = "< 1 min"
-  #   elsif time >= 1 && time < 1.6
-  #           reading_time = "1 min"
-  #   end
-  #   reading_time
-  # end
+  # Estimation of the time to read a post, Merci Em-AK !
+  def get_reading_time(post)
+    words_per_minute = 170.0
+    time = post.word_count / words_per_minute
+    reading_time = "#{time.round} min"
+    if time < 1
+            reading_time = "< 1 min"
+    elsif time >= 1 && time < 1.6
+            reading_time = "1 min"
+    end
+    reading_time
+  end
 
   def get_date(post)
     post.created_at.strftime("%e %b.")
