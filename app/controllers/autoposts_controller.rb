534a5569a7277ec0d3dcb37c7dd766c4bd1deb91
jum-s <vincent.jumeaux@gmail.com>
2014-10-06 23:12:57 +0200

    remove show articles, link_to urls directly

--- app/controllers/autoposts_controller.rb
+++ app/controllers/autoposts_controller.rb
@@ -2,9 +2,4 @@ class AutopostsController < ApplicationController
   def index
     @autoposts = Autopost.order("pocket_date DESC").all
   end
-
-  def show
-    @autopost = Autopost.find(params[:id])
-    redirect_to @autopost.url if has_video(@autopost) == true
-  end
 end
--- app/controllers/twtlinks_controller.rb
+++ app/controllers/twtlinks_controller.rb
@@ -2,9 +2,4 @@ class TwtlinksController < ApplicationController
   def index
     @twtlinks = Twtlink.order("created_at DESC").all
   end
-
-  def show
-    @twtlink = Twtlink.find(params[:id])
-    redirect_to @twtlink.url if has_video(@twtlink) == true
-  end
 end
