2161954fa9badcc9b8582072f69b212a9ac3618f
jum-s <vincent.jumeaux@gmail.com>
2014-11-26 15:28:32 +0100

    select text from the views (ugly)

--- app/controllers/autoposts_controller.rb
+++ app/controllers/autoposts_controller.rb
@@ -1,8 +1,8 @@
 class AutopostsController < ApplicationController
   def index
-    @autoposts = Autopost.order("created_at DESC").select(&:text)
+    @autoposts = Autopost.order(:created_at).page(params[:page]).per(30)
     @autopost_videos = Autopost.order("created_at DESC")
-    				    .select{|auto| auto.url.include?("dailymo") || auto.url.include?("youtu") || auto.url.include?("vimeo")}
-    				    .last 15
+                    				   .select{|auto| auto.url.include?("dailymo") || auto.url.include?("youtu") || auto.url.include?("vimeo")}
+                    				   .last 15
   end
 end
--- app/controllers/twtlinks_controller.rb
+++ app/controllers/twtlinks_controller.rb
@@ -1,6 +1,6 @@
 class TwtlinksController < ApplicationController
   def index
-    @twtlinks = Twtlink.order(:created_at).page(params[:page]).per(6)
+    @twtlinks = Twtlink.order(:created_at).page(params[:page]).per(30)
     @twtlink_videos = Twtlink.order("created_at DESC")
                                            .select{|twt| twt.url.include?("dailymo") || twt.url.include?("youtu") || twt.url.include?("vimeo")}
                                            .last 15  
