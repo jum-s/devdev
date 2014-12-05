9ff9bfd66f16bc615ce706f87b9df1e23c2d5fc3
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2014-12-05 19:39:34 +0200

    application helper wide, and creation api helper

---
+++ app/helpers/api_helper.rb
@@ -0,0 +1,114 @@
+module ApiHelper
+ def twitter_connect
+    baseurl = "https://api.twitter.com"
+    path    = "/1.1/statuses/user_timeline.json"
+    query   = URI.encode_www_form("screen_name" => "Jumijums")
+    address = URI("#{baseurl}#{path}?#{query}")
+    request = Net::HTTP::Get.new address.request_uri
+    http             = Net::HTTP.new address.host, address.port
+    http.use_ssl     = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    consumer_key ||= OAuth::Consumer.new(ENV['TWTSECRET1'], ENV['TWTSECRET2'])
+    access_token ||= OAuth::Token.new(ENV['TWTSECRET4'], ENV['TWTSECRET3'])
+    request.oauth! http, consumer_key, access_token
+    http.start
+    http.request request
+  end
+
+  def framabag_connect
+    feeds = Feedjira::Feed.fetch_and_parse("https://www.framabag.org/u/jumijums/?feed&type=home&user_id=1&token=" + ENV['SECRET_KEY']).entries
+    feeds.map { |feed| feed['url'] }
+  end
+
+####Shared btw Autopost & Twtlink####
+
+  def connect_alchemy
+     AlchemyAPI::Client.new(ENV['ALCHEMY_CONNECT'])
+  end
+
+  def connect_readability(url)
+    begin
+      Readability::Document.new(open(url).read)
+    rescue
+      nil
+    end
+  end
+
+  def get_title(url)
+     connect_alchemy.URLGetTitle(url: url)["title"]
+  end
+  
+  def get_text(url)
+    begin
+      if connect_readability(url) != nil
+        connect_readability(url).content 
+      else
+        ""
+      end
+    rescue
+      ""
+    end
+  end
+  
+  def get_word_count(text)
+    text.split(' ').count
+  end
+  
+  def get_image(url)
+    begin
+      connect_readability(url).images[0] if connect_readability(url) != nil
+    rescue
+      ""
+    end
+  end
+  
+  def get_sentiment(url)
+    begin
+      sentiment_response = connect_alchemy.URLGetTextSentiment(url: url)
+      sentiment_score = sentiment_response["docSentiment"]["score"] if sentiment_response["status"] == "OK"
+      case sentiment_score
+        when 0.05..1 then 1
+        when -0.05..-1 then 3
+        else 2
+      end
+    rescue
+      2
+    end
+  end
+
+  def get_tags(url)
+    begin
+      text_analysis = connect_alchemy.URLGetRankedKeywords(url: url)
+      text_analysis["keywords"].first(10).map {|v| v["text"]}.join(",")
+    rescue
+      ""
+    end
+  end
+
+  def get_language(url)
+    begin
+      text_analysis = connect_alchemy.URLGetRankedKeywords(url: url)
+      text_analysis["language"]
+    rescue
+      ""
+    end
+  end
+
+  def has_a_video(url)
+    url.include?("dailymo") || url.include?("youtu")|| url.include?("vimeo")
+  end
+
+  def clean_url(url)
+    begin
+      uri = URI.parse(url)
+      http = Net::HTTP.new(uri.host)
+      unless url.include? "youtub"
+        response = http.get(uri.path)
+        self.url = response.fetch('location') if response.code != "200" 
+      else
+        self.url = uri.to_s
+      end
+    rescue
+    end
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,41 +1,6 @@
 module ApplicationHelper
-  # Estimation of the time to read a post, Merci Em-AK !
  
- def twitter_connect
-    baseurl = "https://api.twitter.com"
-    path    = "/1.1/statuses/user_timeline.json"
-    query   = URI.encode_www_form("screen_name" => "Jumijums")
-    address = URI("#{baseurl}#{path}?#{query}")
-    request = Net::HTTP::Get.new address.request_uri
-    http             = Net::HTTP.new address.host, address.port
-    http.use_ssl     = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-    consumer_key ||= OAuth::Consumer.new(ENV['TWTSECRET1'], ENV['TWTSECRET2'])
-    access_token ||= OAuth::Token.new(ENV['TWTSECRET4'], ENV['TWTSECRET3'])
-    request.oauth! http, consumer_key, access_token
-    http.start
-    http.request request
-  end
-
-  def fetch_new_urls
-    tweets_ary = JSON.parse(twitter_connect.body) if twitter_connect.code == '200'
-    all_urls = tweets_ary.map{ |t| t["entities"]["urls"][0]}
-                         .compact
-                         .map { |p| p["expanded_url"]}
-                         .compact
-
-    old_urls = Twtlink.all.map(&:url)
-    new_urls = all_urls - old_urls
-  end
-
-  def connect_readability(url)
-    begin
-      Readability::Document.new(open(url).read)
-    rescue
-      nil
-    end
-  end
-
+  # Estimation of the time to read a post, Merci Em-AK !
   def get_reading_time(post)
     words_per_minute = 170.0
     time = post.word_count / words_per_minute
@@ -48,15 +13,11 @@ module ApplicationHelper
     reading_time
   end
 
-  def connect_alchemy
-     AlchemyAPI::Client.new(ENV['ALCHEMY_CONNECT'])
-  end
-
-  def get_date(post)
+  def display_date(post)
     post.created_at.strftime("%e %b.")
   end
 
-  def get_language(post)
+  def display_language(post)
     case post.language 
       when "english" then " | #{image_tag 'uk.png', size: '20x20'}".html_safe
       when "french" then " | #{image_tag 'fr.png', size: '20x20'}".html_safe
@@ -64,18 +25,12 @@ module ApplicationHelper
     end
   end
 
-  def get_sentiment(post)
+  def display_sentiment(post)
     case post.sentiment 
       when 1 then " | #{icon('smile-o')}".html_safe
       when 2 then " | #{icon('meh-o')}".html_safe
       when 3 then " | #{icon('frown-o')}".html_safe
       else
-    end
-  end
-
-  def get_video(post)
-    if post.url.include?("dailymo") || post.url.include?("youtu")
-      post.has_video = true 
     end
   end
 
