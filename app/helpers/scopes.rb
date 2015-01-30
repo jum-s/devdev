f3ba07046aca33df6a0dc0be55e42aa961e48feb
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-01-30 19:21:36 +0200

    fixed has_title scope

--- app/helpers/scopes.rb
+++ app/helpers/scopes.rb
@@ -5,6 +5,6 @@ module Scopes
   
   included  do
     scope :has_video, ->  { where('url like ?', '%youtu%' || '%vimeo%' || '%dailymo%').order(created_at: :desc) }
-    scope :has_title, ->  { where.not(title: nil).order(created_at: :desc) }
+    scope :has_title, ->  { where.not(title: "").order(created_at: :desc) }
   end
 end
