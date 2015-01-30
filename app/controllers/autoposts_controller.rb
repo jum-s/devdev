9512d26310eee7f88a6e156136dca2d5dd227e13
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-01-30 18:58:25 +0200

    DRY controllers

--- app/controllers/autoposts_controller.rb
+++ app/controllers/autoposts_controller.rb
@@ -1,11 +1,6 @@
 class AutopostsController < ApplicationController
   def index
-    if params[:locale] == 'fr'
-      @autoposts = Autopost.has_text.in_french.page(params[:page]).per(30)
-      @autopost_videos = Autopost.has_video.in_french.last 15
-    else
-      @autoposts = Autopost.has_text.in_english.page(params[:page]).per(30)
-      @autopost_videos = Autopost.has_video.in_english.last 15
-    end
+      @autoposts = Autopost.language(params[:locale]).page(params[:page]).per(30)
+      @autopost_videos = Autopost.has_video.language(params[:locale]).last 15
   end
 end
--- app/controllers/twtlinks_controller.rb
+++ app/controllers/twtlinks_controller.rb
@@ -1,14 +1,6 @@
 class TwtlinksController < ApplicationController
   def index
-    if params[:locale] == 'fr'
-      @twtlinks = Twtlink.has_text.in_french.page(params[:page]).per(30)
-      @twtlink_videos = Twtlink.has_video.in_french.last 15
-    elsif params[:locale] == 'en'
-      @twtlinks = Twtlink.has_text.in_english.page(params[:page]).per(30)
-      @twtlink_videos = Twtlink.has_video.in_english.last 15
-    else
-      @twtlinks = Twtlink.has_text.page(params[:page]).per(30)
-      @twtlink_videos = Twtlink.has_video.last 15
-    end
+      @twtlinks = Twtlink.language(params[:locale]).page(params[:page]).per(30)
+      @twtlink_videos = Twtlink.has_video.language(params[:locale]).last 15
   end
 end
