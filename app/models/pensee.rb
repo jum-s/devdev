b5bbf7c7839ea892098a39153216f7a38f97763e
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-02-06 17:27:19 +0200

    refactor pensee

--- app/models/pensee.rb
+++ app/models/pensee.rb
@@ -3,15 +3,12 @@ class Pensee < ActiveRecord::Base
   attr_accessible :text
 
   def create_pensees
-    opened_doc = open('https://raw.githubusercontent.com/jum-s/notes/master/pensees.txt').read
-    parsed_text = Readability::Document.new(opened_doc, :tags => %w[div]).content
+    file = open('https://raw.githubusercontent.com/jum-s/notes/master/pensees.txt').read
+    parsed_text = Readability::Document.new(file, tags: %w[div]).content
     clean_ary = parsed_text.gsub(/<div>/, '')
-                                          .gsub(/<\/div>/, '')
-                                          .split(/\n/).to_s
-    if Pensee.any? == false
-      Pensee.create(text: clean_ary) 
-    else
-      Pensee.update(1, text: clean_ary)
-    end
+                           .gsub(/<\/div>/, '')
+                           .split(/\n/)
+                           .to_s
+    Pensee.count == 0 ? Pensee.create(text: clean_ary) : Pensee.update(1, text: clean_ary)
   end
 end
