f2dada9bbccd3da3108529902777d2e03f77b4d2
Vincent Jumeaux <vincent.jumeaux@gmail.com>
2015-02-06 13:50:08 +0200

    right order desc

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,8 @@ class WelcomeController < ApplicationController
   include LanguageHelper
 
   def index
-    @twtlinks = serve_by_language(Twtlink.all, params[:locale]).first(8)
-    @autoposts = serve_by_language(Autopost.all, params[:locale]).first(8)
+    @twtlinks = serve_by_language(Twtlink.all, params[:locale]).first(10)
+    @autoposts = serve_by_language(Autopost.all, params[:locale]).first(4)
   end
 
   def pensees_and_posts
--- app/helpers/language_helper.rb
+++ app/helpers/language_helper.rb
@@ -2,11 +2,11 @@ module LanguageHelper
   def serve_by_language(obj,locale)
     case locale
     when 'fr'
-      obj.where(language: 'french') 
+      obj.where(language: 'french').order(created_at: :desc) 
     when 'en'
-      obj.where(language: 'english')
+      obj.where(language: 'english').order(created_at: :desc)
     else 
-      obj
+      obj.order(created_at: :desc)
     end
   end
 end
